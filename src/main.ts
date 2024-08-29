42c1ef297368a0325bef73ba6f318a33801f9dda
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-08-28 20:29:53 -0500

    feat:initial-04

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,43 @@
-
-// const listaFrutas: string[] = ['platano','manzana','pera','fresa','naranja','uva'];
-
-// const $listaFrutasDOM = document.getElementById('listadoFrutas') as HTMLUListElement;
-
-// listaFrutas.forEach((fruta) => {
-//    const $item = document.createElement('li');
-//    $item.textContent = fruta;
-//    $listaFrutasDOM.appendChild($item);
-// });
-
-// const listadoFrutas = [
-//    { nombre: 'platano', precio: 1.5},
-//    { nombre: 'manzana', precio: 2 },
-//    { nombre: 'pera', precio: 3 },
-//    { nombre: 'fresa', precio: 4 },
-//    { nombre: 'naranja', precio: 1.5 },
-//    { nombre: 'uva', precio: 2 }
-// ];
-
-// const $listaFrutasDOM = document.getElementById('listadoFrutas') as HTMLUListElement;
-
-// listadoFrutas.forEach((fruta) => {
-//    const $item = document.createElement('li');
-//    $item.textContent = `${fruta.nombre} - ${fruta.precio} `;
-//    $listaFrutasDOM.appendChild($item);
-// });
-
-//Una interfaz en TypeScript se usa para definir la estructura de un objeto, especificando qué propiedades debe tener y de qué tipo son.
-//name: string y precio: number: La interfaz Fruta especifica que cualquier objeto de tipo Fruta debe tener dos propiedades: name y precio.
-
-interface Fruta{
-   nombre: string;
-   precio: number;
+interface User {
+   id: number;
+   name: string;
+   username: string;
+   email: string;
+   address: Address;
+   phone: string;
+   website: string;
+   company: Company;
+}
+ 
+ interface Company {
+   name: string;
+   catchPhrase: string;
+   bs: string;
+ }
+ 
+ interface Address {
+   street: string;
+   suite: string;
+   city: string;
+   zipcode: string;
+   geo: Geo;
+ }
+ 
+interface Geo {
+   lat: string;
+   lng: string;
 }
 
-const listadoFrutas: Fruta[] = [
-   { nombre: 'platano', precio: 1.5},
-   { nombre: 'manzana', precio: 2 },
-   { nombre: 'pera', precio: 3 },
-   { nombre: 'fresa', precio: 4 },
-   { nombre: 'naranja', precio: 1.5 },
-   { nombre: 'uva', precio: 2 }
-];
+const apiList = document.getElementById("apiList") as HTMLUListElement;
 
-const $listaFrutasDOM = document.getElementById('listadoFrutas') as HTMLUListElement;
+fetch("https://jsonplaceholder.typicode.com/users")
+   .then((response) => response.json())
+   .then((users: User[]) => {
+      users.forEach((user) => {
+         const li = document.createElement("li");
+         li.textContent = `${user.name} - ${user.email} - ${user.phone} - ${user.address.city}`;
+         apiList.appendChild(li);
+      });
+   });
 
-listadoFrutas.forEach((fruta) => {
-   const $item = document.createElement('li');
-   $item.textContent = `${fruta.nombre} - ${fruta.precio}`;
-   $listaFrutasDOM.appendChild($item);
-});
+
